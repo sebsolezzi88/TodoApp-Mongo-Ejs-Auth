5866a56a76eb5ef0a7df79f953cf4339720cd0b0
seba <ssolezzi@gmail.com>
2025-08-23 18:30:02 -0300

    Creación del modelo User(usuario)

---
+++ models/User.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true, 
+    unique: true   
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
