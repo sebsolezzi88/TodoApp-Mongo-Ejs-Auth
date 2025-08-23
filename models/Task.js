63d6ab7f45e6523568ce8ee65ab529f811afde12
seba <ssolezzi@gmail.com>
2025-08-23 18:27:57 -0300

    Creación del modelo Task(tarea)

---
+++ models/Task.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const taskSchema = new Schema({
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  text: {
+    type: String,
+    require: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const Task = mongoose.model("Task",taskSchema);
+export default Task;
