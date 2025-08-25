2f816ed91daf1f6bbc7dd6fa451dd0794d1e846a
seba <ssolezzi@gmail.com>
2025-08-25 19:46:09 -0300

    Agregada ruta post /nueva

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { renderDashboard, renderNewTask } from "../controllers/taskController.js";
+import { newTask, renderDashboard, renderNewTask } from "../controllers/taskController.js";
 
 const router = Router();
 
 router.get('/panel',renderDashboard); //Mostrar dashboard, protegida por sessión
 router.get('/nueva',renderNewTask); //Mostrar formulario para ingresar tareas
 
-
+router.post('/nueva',newTask);// Crear nueva tara
 
 
 
