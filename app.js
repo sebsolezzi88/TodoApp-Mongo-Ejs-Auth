fdfae831b49197c7971afebb0a288d9c5b61d284
seba <ssolezzi@gmail.com>
2025-08-25 09:29:44 -0300

    Añadiendo configuración para express session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { getMongoConnection } from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
+import session from 'express-session';
 
 dotenv.config();
 
@@ -9,6 +10,12 @@ const app = express(); //Crear app de express
 const PORT = process.env.PORT || 3000; //Puerto
 
 app.set("view engine", "ejs"); //Motor de plantillas
+app.use(session({ //Uso de express session
+  secret: process.env.SECRET_WORD, 
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false } 
+}));
 app.use(express.urlencoded({ extended: true })); // Para formularios (x-www-form-urlencoded)
 app.use(express.json()); //Para analizar la solicitud de tipo Json
 
