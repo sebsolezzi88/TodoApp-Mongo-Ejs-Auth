370298dfff040679c9fb231d7b571cd506a9f69f
seba <ssolezzi@gmail.com>
2025-08-25 20:59:01 -0300

    Añadira ruta para mostrarl el formulario de actualización

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { deleteTask, newTask, renderDashboard, renderNewTask } from "../controllers/taskController.js";
+import { deleteTask, newTask, renderDashboard, renderEditTask, renderNewTask } from "../controllers/taskController.js";
 
 const router = Router();
 
 router.get('/panel',renderDashboard); //Mostrar dashboard, protegida por sessión
 router.get('/nueva',renderNewTask); //Mostrar formulario para ingresar tareas
+router.get('/editar/:taskId',renderEditTask); //Mostrar formulario para editar tareas
 
-router.post('/nueva',newTask);// Crear nueva tara
+
+router.post('/nueva',newTask);// Crear nueva tarea
 router.delete('/borrar/:taskId',deleteTask); //Ruta para borrar tarea
 
 
