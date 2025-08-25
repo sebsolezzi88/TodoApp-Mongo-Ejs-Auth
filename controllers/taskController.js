b4fd2b7aedef471c39e450709e384796b5b6ad58
seba <ssolezzi@gmail.com>
2025-08-25 19:58:39 -0300

    Funcionalidad para crear tareas

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -36,6 +36,48 @@ export const newTask = async (req, res) => {
 
     const {id,username} = req.session.user; //Extraemos las variables
     console.log(req.body);
+    const {title, text, priority} = req.body;
+    
+    //Comprobar titulo
+    if(!title || title.trim()===''){
+      return res.render("newtask", {
+        alert: { status: "error", msg: "El titulo es requerido." },
+      });
+    }
+    //Comprobar descripcion
+    if(!text || text.trim()===''){
+      return res.render("newtask", {
+        alert: { status: "error", msg: "La descripción es requerida" },
+      });
+    }
+    //Comprobar prioridad
+    if(!priority || priority.trim()===''){
+      return res.render("newtask", {
+        alert: { status: "error", msg: "La prioridad es requerida" },
+      });
+    }
+    //Comprobar prioridad valida
+    const priorities = ['Baja','Media', 'Alta'];
+    if(!priorities.includes(priority)){
+      return res.render("newtask", {
+        alert: { status: "error", msg: "La prioridad no valida" },
+      });
+    }
+    //Guardamos la tarea
+    try {
+      await Task.create({
+        creator:id,
+        title,
+        text,
+        priority
+      });
+      return res.redirect('/tarea/panel'); //Redireccionamos al panel
+    } catch (error) {
+      console.log(error);
+      return res.render("newtask", {
+        alert: { status: "error", msg: "Error en Servidor" },
+      });
+    }
 
   return res.render('newtask');
 };
